7a4cb44efb4a45b2ef23a9c3ae054ff016cb7f50
samyiiitm63952 <samadkhan946148@gmail.com>
2026-03-12 03:21:34 +0530

    Add reverse function for linked list
    Implement a function to reverse a linked list.

---
+++ reverse-linked-list.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* reverse(Node* head){
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* next = NULL;
+
+    while(curr != NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+int main(){
+    cout<<"Reverse Linked List Example";
+}
