276041c8492f5f13e4b34e6bc74eaa8178684b3e
samyiiitm63952 <samadkhan946148@gmail.com>
2026-04-04 19:52:17 +0530

    Time: 8 ms (6.2%), Space: 69.5 MB (92.38%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,25 +4,17 @@ public:
         int maxlen = 0;
         int l = 0, r = 0;
         int zero = 0;
-
-        while (r < nums.size()) {
-
-            // 1. include current element
-            if (nums[r] == 0) zero++;
-
-            // 2. shrink if invalid
-            while (zero > k) {
-                if (nums[l] == 0) zero--;
+        while(r<nums.size()){
+            if(nums[r]==0){
+                zero++;
+            }
+            while(zero>k){
+                if(nums[l]==0)zero--;
                 l++;
             }
-
-            // 3. update answer
-            maxlen = max(maxlen, r - l + 1);
-
-            // 4. move right
+            maxlen =max(maxlen,r-l+1);
             r++;
         }
-
         return maxlen;
     }
 };
