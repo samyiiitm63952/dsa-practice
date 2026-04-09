167e7aaaea10a63349560920cf58f806f2b5cf37
samyiiitm63952 <samadkhan946148@gmail.com>
2026-04-09 20:01:30 +0530

    Time: 14 ms (23.16%), Space: 7.8 MB (64.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    int fibo(int n){
+    int fib(int n){
         if(n==0)return 0;
         if (n==1)return 1;
-      return fibo(n-1)+fibo(n-2);
+      return fib(n-1)+fib(n-2);
     }
-    int fib(int n) {
-        return fibo(n);
-    }
+ 
 };
