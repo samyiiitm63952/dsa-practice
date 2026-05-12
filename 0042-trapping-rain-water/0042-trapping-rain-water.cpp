4fdc77e22e5aec3c926d1a12a91a2425aa9bd0e8
samyiiitm63952 <samadkhan946148@gmail.com>
2026-05-12 20:36:26 +0530

    Time: 0 ms (100%), Space: 26 MB (54.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int total =0;
-        vector<int> prefix(height.size());
-        vector<int>suffix (height.size());
-        prefix[0]=height[0];
-        for(int i=1;i<height.size();i++){
-            prefix[i]=max(prefix[i-1],height[i]);
+
+        int left = 0;
+        int right = height.size() - 1;
+
+        int lmax = 0;
+        int rmax = 0;
+
+        int total = 0;
+
+        while(left < right) {
+
+            if(height[left] < height[right]) {
+
+                if(height[left] >= lmax) {
+                    lmax = height[left];
+                }
+                else {
+                    total += lmax - height[left];
+                }
+
+                left++;
+            }
+
+            else {
+
+                if(height[right] >= rmax) {
+                    rmax = height[right];
+                }
+                else {
+                    total += rmax - height[right];
+                }
+
+                right--;
+            }
         }
-        suffix[height.size()-1]=height[height.size()-1];
-        for(int i=height.size()-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],height[i]);
-        }
-        for(int i=0;i<height.size();i++){
-            total+=min(suffix[i],prefix[i])-height[i];
-        }
+
         return total;
     }
 };
