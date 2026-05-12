853a34d6b50774152f72e0b003bc6c8604bedfab
samyiiitm63952 <samadkhan946148@gmail.com>
2026-05-12 17:09:53 +0530

    Time: 172 ms (5.03%), Space: 27.9 MB (97.38%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans(nums.size(),-1);
         for(int i=0;i<nums.size();i++){
             int j=(i+1)%nums.size();
-            int k=nums.size()-1;
+            int k=nums.size();
             while(k){
                 if(nums[j]>nums[i]){
                     ans[i]=nums[j];
