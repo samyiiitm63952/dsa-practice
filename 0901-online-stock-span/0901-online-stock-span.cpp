18a6033c16ffca09a813357c7fece9897c85e8e5
samyiiitm63952 <samadkhan946148@gmail.com>
2026-05-16 19:23:11 +0530

    Time: 532 ms (5.01%), Space: 92.9 MB (13.1%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -7,8 +7,8 @@ public:
     
     int next(int price) {
         span.push_back(price);
-        int cnt=0;
-        for(int i=span.size()-1;i>=0;i--){
+        int cnt=1;
+        for(int i=span.size()-2;i>=0;i--){
             if(span[i]<=price){
                 cnt++;
             }
