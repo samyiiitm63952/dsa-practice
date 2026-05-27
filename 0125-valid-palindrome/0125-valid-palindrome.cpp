a19e9c87fd43812edbac4cab7373e528a92b03d8
samyiiitm63952 <samadkhan946148@gmail.com>
2026-05-27 22:45:17 +0530

    Time: 0 ms (100%), Space: 10.8 MB (7.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string temp = "";
-
-        for(char c : s){
-            if(isalnum(c)){
-                temp += tolower(c);
+        for(auto it:s){
+            if((it>='a'&& it<='z') ||( it>='A' && it<='Z') ||(it>='0'&& it<='9')){
+                 temp += toupper(it);
             }
+            
+           
         }
+        string str =temp;
+        reverse(str.begin(),str.end());
+        return (str==temp);
 
-        string rev = temp;
-        reverse(rev.begin(), rev.end());
-
-        return temp == rev;
     }
 };
