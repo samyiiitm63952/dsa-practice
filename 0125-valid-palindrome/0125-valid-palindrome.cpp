9899b6cd95c3fb6755dd848e1962b7cb3cf976e4
samyiiitm63952 <samadkhan946148@gmail.com>
2026-05-27 22:56:53 +0530

    Time: 0 ms (100%), Space: 10.3 MB (23.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+    bool helper(string &temp,int n,int i){
+        if(i>=n/2)return true;
+        if(temp[i]!=temp[n-i-1]){
+            return false;
+        }
+        return helper(temp,n,i+1);
+    }
     bool isPalindrome(string s) {
         string temp = "";
         for(auto it:s){
             if((it>='a'&& it<='z') ||( it>='A' && it<='Z') ||(it>='0'&& it<='9')){
                  temp += toupper(it);
-            }
-            
-           
+            }   
         }
-        string str =temp;
-        reverse(str.begin(),str.end());
-        return (str==temp);
+        int n=temp.size();
+        int i=0;
+        return helper(temp,n,i);
 
     }
 };
