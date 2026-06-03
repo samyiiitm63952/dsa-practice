91b849908961bc9539e0a7cb92c8473828db8ce3
samyiiitm63952 <samadkhan946148@gmail.com>
2026-06-03 22:34:28 +0530

    Time: 848 ms (15.74%), Space: 11.8 MB (18.95%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,57 +1,35 @@
 class Solution {
 public:
-
-    bool dfs(vector<vector<char>>& board,
-             string word,
-             int row,
-             int col,
-             int idx){
-
-        // whole word matched
-        if(idx == word.size())
+    vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
+    bool find(int row,int col, vector<vector<char>>& board,string word,int idx){
+        if(idx == word.size()){
             return true;
-
-        // invalid cases
-        if(row < 0 ||
-           col < 0 ||
-           row >= board.size() ||
-           col >= board[0].size() ||
-           board[row][col] != word[idx])
-            return false;
-
-        // save current char
+        }
+        if(row<0||  col<0 || row>=board.size()||col>=board[0].size() || board[row][col]=='$'||board[row][col]!= word[idx] )return false;
         char temp = board[row][col];
+        board[row][col]='$';
+        
+        for(auto &dir:directions){
+            int new_i= row+dir[0];
+            int new_j = col+ dir[1];
 
-        // mark visited
-        board[row][col] = '#';
-
-        // explore 4 directions
-        bool found =
-            dfs(board, word, row+1, col, idx+1) ||
-            dfs(board, word, row-1, col, idx+1) ||
-            dfs(board, word, row, col+1, idx+1) ||
-            dfs(board, word, row, col-1, idx+1);
-
-        // backtrack
-        board[row][col] = temp;
-
-        return found;
+            if(find(new_i,new_j,board,word,idx+1))return true;
+        }
+        board[row][col]=temp;
+        return false;
     }
 
+
     bool exist(vector<vector<char>>& board, string word) {
-
-        int n = board.size();
+        int n= board.size();
         int m = board[0].size();
-
-        for(int i=0; i<n; i++){
-
-            for(int j=0; j<m; j++){
-
-                if(dfs(board, word, i, j, 0))
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((board[i][j]==word[0])&& find(i,j,board,word,0)){
                     return true;
+                }
             }
-        }
-
-        return false;
+         }
+         return false;
     }
 };
