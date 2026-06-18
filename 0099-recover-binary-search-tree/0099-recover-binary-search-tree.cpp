617c7e7c4f0e869ef681b4a0abd9f5fa560ca238
samyiiitm63952 <samadkhan946148@gmail.com>
2026-06-18 20:40:18 +0530

    Time: 0 ms (100%), Space: 61.1 MB (13.28%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -6,36 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<TreeNode*>& ans) {
-        if (root == NULL)
-            return;
-        inorder(root->left, ans);
-        ans.push_back(root);
-        inorder(root->right, ans);
+    void inorder(vector<int>& ans,TreeNode* root){
+        if(root==NULL)return;
+        inorder(ans,root->left);
+        ans.push_back(root->val);
+        inorder(ans,root->right);
+
     }
 
-    void recoverTree(TreeNode* root) {
-        vector<TreeNode*> nodes;
-        inorder(root, nodes);
-        TreeNode* first = nullptr;
-        TreeNode* second = nullptr;
-
-        for (int i = 1; i < nodes.size(); i++) {
-            if (nodes[i]->val < nodes[i - 1]->val) {
-
-                if (!first)
-                    first = nodes[i - 1];
-
-                second = nodes[i];
-            }
+    void solver(vector<int>& sorted,TreeNode * root,int& i){
+        if(root==NULL)return;
+        solver(sorted,root->left,i);
+        if(root->val != sorted[i] ){
+            root->val = sorted[i];
         }
-        swap(first->val, second->val);
-        
+        i++;
+        solver(sorted,root->right,i);
+    }    
+
+    void recoverTree(TreeNode* root) {
+        vector<int>sorted;
+        inorder(sorted,root);
+        sort(sorted.begin(),sorted.end());
+        int i =0;
+        solver(sorted,root,i);
+
     }
 };
