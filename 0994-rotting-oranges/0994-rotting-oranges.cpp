ddd12bf72c58489fddb067189ca4fa562478594d
samyiiitm63952 <samadkhan946148@gmail.com>
2026-06-26 23:38:10 +0530

    Time: 0 ms (100%), Space: 17.1 MB (40.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-
-    queue<pair<pair<int,int>,int>> q;
-
-    int delrow[] = {-1,0,1,0};
-    int delcol[] = {0,1,0,-1};
-
-    int fresh = 0;
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j] == 2)
-                q.push({{i,j},0});
-            else if(grid[i][j] == 1)
-                fresh++;
-        }
-    }
-
-    int time = 0;
-    int rotten = 0;
-
-    while(!q.empty()){
-        auto node = q.front();
-        q.pop();
-
-        int row = node.first.first;
-        int col = node.first.second;
-        int t = node.second;
-
-        time = max(time,t);
-
-        for(int i=0;i<4;i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-
-            if(nrow>=0 && ncol>=0 &&
-               nrow<n && ncol<m &&
-               grid[nrow][ncol]==1)
-            {
-                grid[nrow][ncol] = 2;
-                rotten++;
-                q.push({{nrow,ncol},t+1});
+        vector<pair<int, int>> start;
+        queue<pair<pair<int, int>, int>> q;
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 2) {
+                    start.push_back({i, j});
+                }
             }
         }
-    }
+        int time = 0;
+        for (auto it : start) {
+            q.push({it, time});
+        }
 
-    if(rotten != fresh) return -1;
-    return time;
-}
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                auto curr = q.front().first;
+                int curtime = q.front().second;
+
+                q.pop();
+
+                for (int i = 0; i < 4; i++) {
+                    int nrow = curr.first + delrow[i];
+                    int ncol = curr.second + delcol[i];
+
+                    if (nrow >= 0 && ncol >= 0 && nrow < grid.size() &&
+                        ncol < grid[0].size() && grid[nrow][ncol] == 1) {
+                        grid[nrow][ncol] = 2;
+                        q.push({{nrow, ncol}, curtime+1});
+                    }
+                }
+                time = max(curtime, time);
+            }
+        }
+
+        for (auto row : grid) {
+            for (auto col : row) {
+                if (col == 1)
+                    return -1;
+            }
+        }
+        return time;
+    }
 };
